3c7442265429c2b727f83c5d00c7b0ae7274cae4
Martin Atsinov <martin.atsinov@isobar.com>
2017-11-28 10:04:32 +0200

    update startup and config

--- data/db.js
+++ data/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const package = require('../package.json')
+const env = process.env.NODE_ENV || 'development'
 
 mongoose.Promise = require('bluebird')
-mongoose.connect(package.config.dbPath, { useMongoClient: true })
+mongoose.connect(package.config.dbPath[env], { useMongoClient: true })
   .then(result => console.log('\x1b[33m%s', `[Mongo] Connected to ${package.config.dbPath}`))
   .catch(err => console.warn('\x1b[31m%s', `[Mongo] ${err}`))
 
